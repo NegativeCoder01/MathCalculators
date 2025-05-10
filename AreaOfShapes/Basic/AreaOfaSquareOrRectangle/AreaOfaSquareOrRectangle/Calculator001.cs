0754aacc32e325a53146e3f0d43a876f7383bb0d
NegativeCoder01 <Negativestudiomain@gmail.com>
2025-05-09 18:49:40 -0600

    Adds Area of Square or Rectangle

---
+++ AreaOfShapes/Basic/AreaOfaSquareOrRectangle/AreaOfaSquareOrRectangle/Calculator001.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        console.Write("Enter the width: ");
+        double width = Convert.ToDouble(Console.ReadLine());
+
+        console.Write("Enter the height: ");
+        double height = Conver.ToDouble(Console.ReadLine());
+
+        if (width == height)
+        {
+            string shapeName = "cube";
+        }
+        else
+        {
+            string shapeName = "rectangle";
+        }
+
+        double area = width*height;
+
+        Console.WriteLine($"The area of your {shapeName} is: {area}");
+    }
+}
